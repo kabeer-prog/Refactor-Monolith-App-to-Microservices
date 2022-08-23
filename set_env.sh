dac46de2413937fe3ecd26677bf957abeb221a06
kabeer-prog <kabeerlap@gmail.com>
2022-08-23 05:17:44 +0100

    Added starter files and deploy code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdb
+export POSTGRES_PASSWORD=udagramdb
+export POSTGRES_HOST=udagramdb.ckhnestvbko6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=kabeers3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
